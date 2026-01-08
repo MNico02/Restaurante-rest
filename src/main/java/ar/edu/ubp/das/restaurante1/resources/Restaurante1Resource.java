b9f5d82095d42c2f5703eda98a208232fb13d791
MNico02 <94639497+MNico02@users.noreply.github.com>
2026-01-08 09:38:53 -0300

    agrege documentacion

--- src/main/java/ar/edu/ubp/das/restaurante1/resources/Restaurante1Resource.java
+++ src/main/java/ar/edu/ubp/das/restaurante1/resources/Restaurante1Resource.java
@@ -18,6 +18,12 @@ public class Restaurante1Resource {
     @Autowired
     private Restaurante1Repository restaurante1Repository;
 
+    /**
+     * guarda en la base de datos del restaurante la reserva que le mando ristorino
+     * Recive un reservaBean y devuelvel el codigo de reserva(String)
+     * @param reserva
+     * @return codReserva
+     */
     @PostMapping("/confirmarReserva")
     public ResponseEntity<Map<String, String>> insertarReserva(@RequestBody ReservaBean reserva) {
         String codReserva = restaurante1Repository.insReserva(reserva);
@@ -25,6 +31,12 @@ public class Restaurante1Resource {
         response.put("codReserva", codReserva);
         return ResponseEntity.ok(response);
     }
+
+    /**
+     * devuelve a ristorino una lista de horarios que dependen de la solicitud hecha por ristorino
+     * @param soliHorarioBean
+     * @return
+     */
     @GetMapping("/consultarDisponibilidad")
     public ResponseEntity<List<HorarioBean>> obtenerHorarios(@RequestBody SoliHorarioBean soliHorarioBean) {
         List<HorarioBean> horarios = restaurante1Repository.getHorarios(soliHorarioBean);
@@ -32,13 +44,22 @@ public class Restaurante1Resource {
     }
 
 
-
+    /**
+     * le da a ristorino toda la info del restaurante
+     * @param id
+     * @return info
+     * @throws JsonProcessingException
+     */
     @GetMapping("/restaurante")
     public ResponseEntity<RestauranteBean> getRestaurante(@RequestParam("id") int id) throws JsonProcessingException {
         RestauranteBean info = restaurante1Repository.getInfoRestaurante(id);
         return ResponseEntity.ok(info);
     }
-
+    /**
+     * inserta en la base de datos una lista de clicks que ristorino le envia por medio del proceso batch.
+     * @param clicks
+     * @return success true o succes false
+     */
     @PostMapping("/registrarClicks")
     public ResponseEntity<Map<String, Object>> insertarCicks(@RequestBody List<SoliClickBean> clicks) {
         try {
